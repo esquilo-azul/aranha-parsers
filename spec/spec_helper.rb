42ca5ff7ef6c5579e5734e10d6baefd1ef73e60f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-06-28 12:54:42 -0300

    spec/**/*.rb: adapt to "eac_ruby_gem_support" '0.2.0'.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,7 @@ require 'tmpdir'
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ::File.join(::Dir.tmpdir, 'aranha-parsers_rspec')
+
+  require 'eac_ruby_gem_support/rspec'
+  ::EacRubyGemSupport::Rspec.setup(::File.expand_path('..', __dir__), config)
 end
