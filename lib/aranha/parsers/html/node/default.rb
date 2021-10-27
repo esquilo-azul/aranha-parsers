589842835a9436eba33e4f7bf44fc90fb426449f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-10-27 19:14:07 -0300

    Aranha::Parsers::Html::Node::Default::NumericSupport: extract from Aranha::Parsers::Html::Node::Default.

--- lib/aranha/parsers/html/node/default.rb
+++ lib/aranha/parsers/html/node/default.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 require 'aranha/parsers/html/node/base'
+require 'eac_ruby_utils/core_ext'
 
 module Aranha
   module Parsers
     module Html
       module Node
         class Default < ::Aranha::Parsers::Html::Node::Base
+          require_sub __FILE__, include_modules: true
+
           def string_value(node, xpath)
             if node.at_xpath(xpath)
               sanitize_string(node.at_xpath(xpath).text)
@@ -43,30 +46,6 @@ module Aranha
             ''
           end
 
-          def integer_value(node, xpath)
-            r = string_value(node, xpath)
-            return nil if r.blank?
-
-            m = /\d+/.match(r)
-            raise "Integer not found in \"#{r}\"" unless m
-
-            m[0].to_i
-          end
-
-          def integer_optional_value(node, xpath)
-            r = string_value(node, xpath)
-            m = /\d+/.match(r)
-            m ? m[0].to_i : nil
-          end
-
-          def float_value(node, xpath)
-            parse_float(node, xpath, true)
-          end
-
-          def float_optional_value(node, xpath)
-            parse_float(node, xpath, false)
-          end
-
           def array_value(node, xpath)
             r = node.xpath(xpath).map { |n| n.text.strip }
             r.join('|')
@@ -94,16 +73,6 @@ module Aranha
           end
 
           private
-
-          def parse_float(node, xpath, required)
-            s = string_value(node, xpath)
-            m = /\d+(?:[\.\,](\d+))?/.match(s)
-            if m
-              m[0].delete('.').tr(',', '.').to_f
-            elsif required
-              raise "Float value not found in \"#{s}\""
-            end
-          end
 
           def sanitize_string(obj)
             obj.to_s.tr("\u00A0", ' ').strip
