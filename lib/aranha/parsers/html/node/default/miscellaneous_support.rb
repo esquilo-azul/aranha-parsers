3e708d13c0c300e0a81505c253c7e4e0a5868548
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2023-05-30 23:26:04 -0300

    Aranha::Parsers::Html::Node::Default::MiscellaneousSupport.

---
+++ lib/aranha/parsers/html/node/default/miscellaneous_support.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'aranha/parsers/html/node/base'
+require 'eac_ruby_utils/core_ext'
+
+module Aranha
+  module Parsers
+    module Html
+      module Node
+        class Default < ::Aranha::Parsers::Html::Node::Base
+          module MiscellaneousSupport
+            # @param node [Nokogiri::XML::Node]
+            # @param xpath [String]
+            # @return [Boolean]
+            def boolean_value(node, xpath)
+              node_value(node, xpath).to_bool
+            end
+          end
+        end
+      end
+    end
+  end
+end
